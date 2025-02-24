6be53c3d65765a9217eadea20c98541a0a0fb586
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-02-24 10:02:50 +0530

    Add files via upload

---
+++ Comments/Single_line_comments.sh
@@ -0,0 +1,1 @@
+# This is  a Single line comment " # " it is used to Put single line comment in bash
---
+++ Comments/multiline_comment.sh
@@ -0,0 +1,4 @@
+: <<Comment
+This is a multi-line comment in Bash.
+You can write multiple lines here.
+Comment
