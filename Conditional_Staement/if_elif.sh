1c85dbcde952362ef7a194d4712bda99643ef8f6
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-02-25 15:23:07 +0530

    Add files via upload

---
+++ Conditional_Staement/if_elif.sh
@@ -0,0 +1,11 @@
+a=9
+
+if (( $a < 9 ))
+then 
+    echo " The Condition is False "
+elif (( $a <= 9 ))
+then 
+echo "the value of a is 9 "
+else
+echo "The Condition is True "
+fi
---
+++ Conditional_Staement/if_else.sh
@@ -0,0 +1,9 @@
+a=10
+if [ $a -eq 9 ]
+then 
+    echo " A is Equal to 10 " 
+
+else 
+    echo "A is not equal to 10 "
+
+fi
