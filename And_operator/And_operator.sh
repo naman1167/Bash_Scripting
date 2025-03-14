f9348e944436e1194efaec1fba698c56ea9632ce
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-03-14 23:24:04 +0530

    Add files via upload

---
+++ And_operator/And_operator.sh
@@ -0,0 +1,9 @@
+age=50
+
+if (( age > 18 )) && (( age < 40 ))
+then
+    echo " Age is correct !!!  "
+
+else 
+echo " The Age is Incorrect "
+fi
---
+++ Athmetic_and_Numbers/arthmatic_numbers.sh
@@ -0,0 +1,10 @@
+echo "Enter First Number : "
+read n1 
+echo "Enter Second Number : "
+read n2
+
+
+echo $(expr $n1 + $n2 )
+echo $(expr $n1 - $n2 )
+echo $(expr $n1 / $n2 )
+echo $(expr $n1 \* $n2 )
